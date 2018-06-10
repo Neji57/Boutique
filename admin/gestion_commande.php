4998333c1e896d29466ac1011c0483abcb55455b
Neji57 <steve.toffano@gmail.com>
2018-06-10 15:22:48 +0200

    #

--- admin/gestion_commande.php
+++ admin/gestion_commande.php
@@ -2,48 +2,61 @@
 
 require_once('inc/header.php');
 
-$resultat = $pdo->query("SELECT id_commande, c.id_membre, montant, date_enregistrement, etat, pseudo, adresse, ville, code_postal FROM commande AS c, membre AS m");
-$commandes = $resultat->fetchAll();
+if(userAdmin())
+{
+    $resultat = $pdo->query("SELECT id_commande, c.id_membre, montant, date_enregistrement, etat, pseudo, adresse, ville, code_postal FROM commande AS c, membre AS m");
+    $commandes = $resultat->fetchAll();
 
-$ca = "<p>Total du chiffre d'affaires pour les commandes en cours: ";
+    $ca = "<p>Total du chiffre d'affaires pour les commandes en cours: ";
 
-$contenu .= "<table class='table'>";
-$contenu .= "<thead><tr>";
+    $contenu .= "<table class='table'>";
+    $contenu .= "<thead><tr>";
 
-for ($i = 0; $i < $resultat->columnCount(); $i++) {
-    $champs = $resultat->getColumnMeta($i);
-    $contenu .= "<th>" . $champs['name'] . "</th>";
-}
-$contenu .= "</tr></thead><tbody>";
-$contenu .= "</tr></thead><tbody>";
-foreach ($commandes as $commande) {
-    $contenu .= "<tr>";
-    foreach ($commande as $key => $value) {
-        
-            $contenu .= "<td>" . $value . "</td>";
-        
+    for ($i = 0; $i < $resultat->columnCount(); $i++) 
+    {
+        $champs = $resultat->getColumnMeta($i);
+        $contenu .= "<th>" . $champs['name'] . "</th>";
     }
-    
-    $contenu .= "</tr>";
+    $contenu .= "</tr></thead><tbody>";
+    $contenu .= "</tr></thead><tbody>";
+    foreach ($commandes as $commande) 
+    {
+        $contenu .= "<tr>";
+        foreach ($commande as $key => $value) 
+        {
+            
+                $contenu .= "<td>" . $value . "</td>";
+            
+        }
+        
+        $contenu .= "</tr>";
+    }
+    $contenu .= "</tbody></table>";
+
+
+
+    // créer une boucle pour ajouter la somme des montants dans $ca
+
+    $ca .= "</p>";
+
+    // debug($champs);
+
 }
-$contenu .= "</tbody></table>";
-
-
-
-// créer une boucle pour ajouter la somme des montants dans $ca
-
-$ca .= "</p>"
-
-// debug($champs);
+else
+{
+    header('location:../index.php');
+}
 
 ?>
+
+
+
 
 <h1>Gestion des commandes</h1>
 
 <?= $contenu ?>
 <!-- ajouter un formulaire select et un bouton valider pour l'état de la commande-->
 <?= $ca ?>
-
 
 
 <?php require_once('inc/footer.php'); ?>
